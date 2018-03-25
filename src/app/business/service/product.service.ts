f97e07d18d3e0b24f7c11b8eba6e0f54bedfac3a
Taras Shpek <taras.shpek@gmail.com>
2018-03-25 12:06:33 +0300

    Fix product service

--- src/app/business/service/product.service.ts
+++ src/app/business/service/product.service.ts
@@ -8,38 +8,29 @@ export class ProductService {
   constructor() { }
 
   getProducts(): Product[] {
-    return [this.createProduct1(), this.createProduct2(), this.createProduct3()];
+    // mocked products
+    return [
+      {
+        category: Category.MENS,
+        description: 'Some desc',
+        isAvailable: true,
+        name: 'Adidas',
+        price: 999
+      },
+      {
+        category: Category.WOMENS,
+        description: 'Some other desc',
+        isAvailable: false,
+        name: 'Nike',
+        price: 888
+      },
+      {
+        category: Category.WOMENS,
+        description: 'Some other desc',
+        isAvailable: true,
+        name: 'Puma',
+        price: 888
+      }
+    ];
   }
-
-  private createProduct1(): Product {
-    const product1 = new Product();
-    product1.category = Category.MENS;
-    product1.description = 'Some desc';
-    product1.isAvailable = true;
-    product1.name = 'Adidas';
-    product1.price = 999;
-    return product1;
-  }
-
-  private createProduct2(): Product {
-    const product2 = new Product();
-    product2.category = Category.WOMENS;
-    product2.description = 'Some other desc';
-    product2.isAvailable = false;
-    product2.name = 'Nike';
-    product2.price = 888;
-    return product2;
-  }
-
-  private createProduct3(): Product {
-    const product3 = new Product();
-    product3.category = Category.WOMENS;
-    product3.description = 'Some other desc';
-    product3.isAvailable = true;
-    product3.name = 'Puma';
-    product3.price = 888;
-    return product3;
-  }
-
-
 }
